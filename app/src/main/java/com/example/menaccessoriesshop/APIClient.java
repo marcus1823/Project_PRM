d8e60abc24aeec90d5996d6dbb1f9ee7923f201c
Truong Minh Tien <145823751+marcus1823@users.noreply.github.com>
2025-03-23 22:29:53 +0700

    fix error

--- app/src/main/java/com/example/menaccessoriesshop/APIClient.java
+++ app/src/main/java/com/example/menaccessoriesshop/APIClient.java
@@ -2,7 +2,7 @@ package com.example.menaccessoriesshop;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
+// 123
 public class APIClient {
     private static String baseURL = "https://67df923a7635238f9aa9ee49.mockapi.io/";
     private static Retrofit retrofit;
